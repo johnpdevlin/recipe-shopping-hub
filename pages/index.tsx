f70278ea7f7690de55dd1d7880c2f893195b7d77
John Devlin <johnp.devlin2@gmail.com>
2023-01-12 09:49:23 +0000

    First commit - unneeded BP code removed

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Recipe / Shopping Hub</title>
+				<meta
+					name='description'
+					content='An app to manage recipes and your shopping list for the week ahead.'
+				/>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+		</>
+	);
+}
